667fb20f888e5427b3d6c103863c7a3eee86add9
Shreelaxmi Gangolli <shreelaxmi.g@sonata-software.com>
2025-02-22 22:59:42 +0530

    Initial commit for CLI_User project

--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda"
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DeployStack extends cdk.Stack {
@@ -7,6 +8,24 @@ export class DeployStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const lambdaFunction = new lambda.Function(this,"LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code:lambda.Code.fromAsset("lambda"),
+      handler:"main.handler",
+    });
+
+    const functionUrl = lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors:{
+        allowedOrigins:["*"],
+        allowedMethods:[lambda.HttpMethod.ALL],
+        allowedHeaders:["*"],
+      },
+    }) ;
+
+    new cdk.CfnOutput(this,"Url",{
+      value: functionUrl.url,
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'DeployQueue', {
